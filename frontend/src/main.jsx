a2814017aff0f076f01625f85ab06be2aa0b7c9c
devasol <dawit8908@gmail.com>
2025-11-22 16:35:33 +0300

    Blank space in the contact page fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,4 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
